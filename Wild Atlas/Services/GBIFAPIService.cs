ca4104d23721f40fa351404be179a29a0ca9deae
Louis Kuster <macbeth574@gmail.com>
2025-11-22 23:13:41 +0100

    Added new GBIFAPIService.cs

---
+++ Wild Atlas/Services/GBIFAPIService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wild_Atlas.Services
+{
+    public class GBIFAPIService
+    {
+    }
+}
