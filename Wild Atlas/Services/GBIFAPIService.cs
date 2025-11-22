2c8644836a9332da206429729e609d49a3186f71
Louis Kuster <macbeth574@gmail.com>
2025-11-22 23:34:18 +0100

    Added new private method GetTaxonKey in GBIFAPIService

--- Wild Atlas/Services/GBIFAPIService.cs
+++ Wild Atlas/Services/GBIFAPIService.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using static Java.Text.Normalizer;
 
 namespace Wild_Atlas.Services
 {
     public class GBIFAPIService
     {
+        private readonly string _baseUrl = "https://api.gbif.org/v1/";
+
+        private async Task<int?> GetTaxonKey(string commonName)
+        {
+            HttpClient client = new HttpClient();
+
+            string encoded = Uri.EscapeDataString(commonName);
+
+            string url =
+                $"https://api.gbif.org/v1/species/search?q={encoded}" +
+                "&qField=VERNACULAR" +
+                "&rank=SPECIES" +
+                "&status=ACCEPTED" +
+                "&datasetKey=d7dddbf4-2cf0-4f39-9b2a-bb099caae36c";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            JsonDocument doc = JsonDocument.Parse(json);
+            JsonElement root = doc.RootElement;
+
+            JsonElement results = root.GetProperty("results");
+
+            if (results.GetArrayLength() == 0)
+                return null;
+
+            JsonElement first = results[0];
+
+            int key = first.GetProperty("key").GetInt32();
+
+            return key;
+        }
     }
 }
