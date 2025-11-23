66a2d0dbe66ffec1ba0d576a38a0c3593c08ddc8
Louis Kuster <macbeth574@gmail.com>
2025-11-23 19:31:36 +0100

    Added new CheckItem model

---
+++ Wild Atlas/Models/CheckItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Wild_Atlas.Models
+{
+    public partial class CheckItem : ObservableObject
+    {
+        [ObservableProperty]
+        private string label;
+
+        [ObservableProperty]
+        private bool isChecked;
+    }
+}
