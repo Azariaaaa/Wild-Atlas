c6f8ee683446478fb037c104c4f7aa72638c5069
Louis Kuster <macbeth574@gmail.com>
2025-11-25 00:15:09 +0100

    Added new Departement model

---
+++ Wild Atlas/Models/Departement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wild_Atlas.Models
+{
+    class Departement
+    {
+        public string Code { get; set; }
+        public string Nom { get; set; }
+    }
+}
