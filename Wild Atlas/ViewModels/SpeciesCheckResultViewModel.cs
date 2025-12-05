0514a45b00a2ed8491357d9fc92a5ed68d4363da
Louis Kuster <macbeth574@gmail.com>
2025-12-06 00:00:51 +0100

    Added new SpeciesCheckResultViewModel

---
+++ Wild Atlas/ViewModels/SpeciesCheckResultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Wild_Atlas.Models;
+
+namespace Wild_Atlas.ViewModels
+{
+    public partial class SpeciesCheckResultViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private SpeciesCheckFormData formData;
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.TryGetValue("FormData", out var value))
+            {
+                FormData = value as SpeciesCheckFormData;
+            }
+        }
+    }
+}
